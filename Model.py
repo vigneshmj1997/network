faba3ef7ed3f62b6cb975d2e19471dd6464ed8ad
vignesh.sathiyakumar <vignesh.sathiyakumar@zohocorp.com>
2020-11-23 22:25:56 +0530

    done

--- Model.py
+++ Model.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 class Project(nn.Module):
     def __init__(self):
         super(Project,self).__init__()
-        self.conv = nn.Conv1d(1,2,3)
+        self.conv = nn.Conv1d(1,2,3,padding=1)
         self.pool = nn.MaxPool1d(2)
-        self.conv1 = nn.Conv1d(2,4,2)
+        self.conv1 = nn.Conv1d(2,4,2,padding=1)
         self.linear1 = nn.Linear(3*4,8)
         self.linear2 = nn.Linear(8,4)
         self.linear3 = nn.Linear(4,2)
