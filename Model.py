15b57efc4f1b288ea6abf430b4308c3ce6a20679
vignesh.sathiyakumar <vignesh.sathiyakumar@zohocorp.com>
2020-11-25 10:16:13 +0530

    made chanages in model

--- Model.py
+++ Model.py
@@ -1,19 +1,23 @@
 import torch.nn as nn
 
+
 class Project(nn.Module):
     def __init__(self):
-        super(Project,self).__init__()
-        self.conv = nn.Conv1d(1,2,3,padding=1)
+        super(Project, self).__init__()
+        self.conv = nn.Conv1d(1, 2, 3, padding=1)
         self.pool = nn.MaxPool1d(2)
-        self.conv1 = nn.Conv1d(2,4,2,padding=1)
-        self.linear1 = nn.Linear(3*4,8)
-        self.linear2 = nn.Linear(8,4)
-        self.linear3 = nn.Linear(4,2)
-        
+        self.relu = nn.ReLU()
+        self.conv1 = nn.Conv1d(2, 4, 2, padding=1)
+        self.linear1 = nn.Linear(3 * 4, 8)
+        self.linear2 = nn.Linear(8, 4)
+        self.linear3 = nn.Linear(4, 2)
+
     def forward(self, input):
-        x = self.conv(input.view(len(input),1,10))
+
+        x = self.conv(input.view(len(input), 1, 10))
         x = self.pool(x)
         x = self.conv1(x)
-        x = x.view(-1,3*4)
-        x = self.linear3(self.linear2(self.linear1(x)))
+        x = self.pool(x)
+        x = x.view(-1, 3 * 4)
+        x = self.relu(self.linear3(self.relu(self.linear2(self.relu(self.linear1(x))))))
         return x
